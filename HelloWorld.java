cd98d02ff00393d5f73bc964d8bb50bee06bbe0f
Sahil Babbar <124916680+SahilBabbar12@users.noreply.github.com>
2023-02-19 20:55:33 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Program Executed..");
+        System.out.println("Successfully");
     }
 }
